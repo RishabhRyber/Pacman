af918359680f04dc32c0f208ee7185be9b6b7b7e
rishabh <rishabhryber@gmail.com>
2020-04-10 23:11:26 +0530

    game almost setup enemy movement left

--- temp.c
+++ temp.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <GL/glut.h>
-#include <math.h>
+#include <string.h>
 
 #define row 10
 #define col 20
@@ -12,18 +12,18 @@ int placement[row][col];
 int player_x,player_y;
 int score=0;
 /*
+    -2 indicates horizontal walls
+    -1 indicates vertical walls
     0 indicates neutral
     1 indicates food
     2 indicates enemies
     3 indicate player
-    -1 indicates walls
 
 */
 void initialize_pos(){
     for(int i=0;i<row;i++)
         for(int j=0;j<col;j++){
             placement[i][j]=1;
-            printf("lol%d ",placement[i][j]);      
         }
         placement[0][0]=3;
         player_x=0;
@@ -31,9 +31,26 @@ void initialize_pos(){
         placement[5][5]=2;
         placement[3][4]=2;
         placement[1][7]=2;
-        placement[9][2]=2;
-
+        placement[8][2]=-1;
+        placement[1][4]=-1;
+        placement[2][4]=-2;
+        placement[2][5]=-2;
+        placement[2][6]=-2;
+        placement[2][7]=-2;
+        
 }
+
+
+
+void moveEnemies(){
+    for(int i=0;i<row;i++){
+        for (int  j = 0; i < col; j++)
+        {
+        }
+        
+    }
+}
+
 
 /* Note: x axis  extends column wise
          y axis extends row wise
@@ -68,6 +85,40 @@ void showFood(int i,int j){
     glEnd();
 }
 
+void showWall(int i,int j){
+    glColor3f(1,1,0);
+    if(placement[i][j]==-1){
+        glLineWidth(10);
+        glBegin(GL_LINES);
+        glVertex2d(j*6+3,i*6+1);
+        glVertex2d(j*6+3,(i+1)*6-1);
+        glEnd();
+    }else{
+        glLineWidth(10);
+        glBegin(GL_LINES);
+        glVertex2d(j*6+3,i*6+3);
+        glVertex2d((j+1)*6,i*6+3);
+        glEnd();
+    }
+}
+
+void showScoreBoard(){
+
+    //Title
+    glRasterPos2f(47,11*6);
+    char *k=" P A C M A N ";
+    for(int i=0;i<strlen(k);i++)
+        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24 , k[i]);
+
+    //Score Part
+    char s[15]={'S','C','O','R','E',':',' '};
+    sprintf(s+7,"%d",score);
+    int t = strlen(s);
+    glRasterPos2f(6*8,10*6);
+    for(int i=0;i<t;i++)
+        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, s[i]);
+}
+
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT);
@@ -76,16 +127,18 @@ void display()
         for(int j=0;j<col;j++){
             if(placement[i][j]==1){
                 showFood(i,j);
-                printf("lol\n");
             }
             else if(placement[i][j]==3){
                 showPlayer(i,j);
             }else if(placement[i][j]==2){
                 showEnemy(i,j);
+            }else if(placement[i][j]==-1||placement[i][j]==-2){
+                showWall(i,j);
             }
         }
 
     }
+    showScoreBoard();
     glFlush();
 }
 
@@ -94,16 +147,12 @@ void init()
     glViewport(0, 0, 500, 500);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluOrtho2D(0, 120.0, 0, 60.0);
+    gluOrtho2D(0, 120.0, 0, 70.0);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     glColor3f(0.5, 0.5, 1.0);
     glClearColor(0.0, 0.0, 0.0, 0.0);
 }
-
-
-
-
 
 /*
      a->lefts
@@ -129,7 +178,7 @@ void mykeys(unsigned char key, int x, int y)
             placement[player_y][player_x]=0;
             player_y++;
             placement[player_y][player_x]=3;
-        }else if(placement[player_y+1][player_x]==-1){
+        }else if(placement[player_y+1][player_x]==-1 || placement[player_y+1][player_x]==-2){
             // Wall on upper block
             //do nothing for now
         }else if(placement[player_y+1][player_x]==2){
@@ -151,7 +200,7 @@ void mykeys(unsigned char key, int x, int y)
             placement[player_y][player_x]=0;
             player_y--;
             placement[player_y][player_x]=3;
-        }else if(placement[player_y-1][player_x]==-1){
+        }else if(placement[player_y-1][player_x]==-1 || placement[player_y-1][player_x]==-2){
             // Wall on lower block
             //do nothing for now
         }else if(placement[player_y-1][player_x]==2){
@@ -173,7 +222,7 @@ void mykeys(unsigned char key, int x, int y)
             placement[player_y][player_x]=0;
             player_x++;
             placement[player_y][player_x]=3;
-        }else if(placement[player_y][player_x+1]==-1){
+        }else if(placement[player_y][player_x+1]==-1 || placement[player_y][player_x+1] == -2){
             // Wall on right block
             //do nothing for now
         }else if(placement[player_y][player_x+1]==2){
@@ -195,7 +244,7 @@ void mykeys(unsigned char key, int x, int y)
             placement[player_y][player_x]=0;
             player_x--;
             placement[player_y][player_x]=3;
-        }else if(placement[player_y][player_x-1]==-1){
+        }else if(placement[player_y][player_x-1]==-1 || placement[player_y][player_x-1]==-2){
             // Wall on right block
             //do nothing for now
         }else if(placement[player_y][player_x-1]==2){
@@ -205,7 +254,7 @@ void mykeys(unsigned char key, int x, int y)
         break;
 
     }
-    moveEmemies();
+    moveEnemies();
     glutPostRedisplay();
 }
 
@@ -214,7 +263,7 @@ int main(int argc, char **argv)
     initialize_pos();
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
-    glutInitWindowSize(1200,600);
+    glutInitWindowSize(1200,650);
     glutCreateWindow("Transformation with out APIs");
     glutDisplayFunc(display);
     glutKeyboardFunc(mykeys);
